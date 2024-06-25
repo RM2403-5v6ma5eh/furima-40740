8aa653852911d99ec606977a5fb85f7f04bab6bd
RM2403-5v6ma5eh <r.m.2403@it-craftsman-kazuki.jp>
2024-06-25 20:07:23 +0900

    ユーザー管理機能（コミット６）

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 2) }
     email                 {Faker::Internet.email}
-    password              {Faker::Internet.password(min_length: 6) }
+    password              {'1a'+ Faker::Internet.password(min_length: 6) }
     password_confirmation {password}
     last_name             {Faker::JapaneseName.last_name}
     first_name            {Faker::JapaneseName.first_name}
@@ -11,17 +11,4 @@ FactoryBot.define do
     first_name_kana       {Faker::JapaneseName.first_name_kana}
     birthday              { Faker::Date.birthday(min_age: 18, max_age: 65) }
   end
-  
-#  factory :user do
-#    nickname              {'furima村上竜一'}
-#    email                 {'r.m.2403@it-craftsman-kazuki.jp'}
-#    password              {'000000'}
-#    password_confirmation {password}
-#    last_name             {'村上'}
-#    first_name            {'竜一'}
-#    last_name_kana        {'ムラカミ'}
-#    first_name_kana       {'リュウイチ'}
-#    birthday              {1964-07-11}
-#  end
-
-end
+ end
