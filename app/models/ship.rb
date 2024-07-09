8a177c7b5cb16194cdef1151476462faae9127d9
RM2403-5v6ma5eh <r.m.2403@it-craftsman-kazuki.jp>
2024-07-09 22:43:51 +0900

    商品購入機能（コミット１）

---
+++ app/models/ship.rb
@@ -0,0 +1,5 @@
+class Ship < ApplicationRecord
+  belongs_to :order
+  belongs_to_active_hash :region
+
+ end
---
+++ db/migrate/20240709115643_create_ships.rb
@@ -0,0 +1,14 @@
+class CreateShips < ActiveRecord::Migration[7.0]
+  def changestreet_address
+    create_table :ships do |t|
+      t.string     :post_code,              null:  false
+      t.integer    :region_id,              null:  false     
+      t.string     :municipality,           null:  false  
+      t.string     :street_address,         null:  false
+      t.string     :build_name            
+      t.string     :phone_num,              null:  false
+      t.references :order,                  null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
